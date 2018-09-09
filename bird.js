e5a18bd1c07360553ce0b540530d002f9124f496
Rinzler786 <40955135+Rinzler786@users.noreply.github.com>
2018-09-09 14:52:26 +0100

    Update bird.js

--- bird.js
+++ bird.js
@@ -1,3 +1,4 @@
+// Mutation function to be passed into bird.brain
 function mutate(x) {
   if (random(1) < 0.1) {
     let offset = randomGaussian() * 0.5;
@@ -39,3 +40,68 @@ class Bird {
   copy() {
     return new Bird(this.brain);
   }
+
+  // Display the bird
+  show() {
+    fill(255, 100);
+    stroke(255);
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+
+  // This is the key function now that decides
+  // if it should jump or not jump!
+  think(pipes) {
+    // First find the closest pipe
+    let closest = null;
+    let record = Infinity;
+    for (let i = 0; i < pipes.length; i++) {
+      let diff = pipes[i].x - this.x;
+      if (diff > 0 && diff < record) {
+        record = diff;
+        closest = pipes[i];
+      }
+    }
+
+    if (closest != null) {
+      // Now create the inputs to the neural network
+      let inputs = [];
+      // x position of closest pipe
+      inputs[0] = map(closest.x, this.x, width, 0, 1);
+      // top of closest pipe opening
+      inputs[1] = map(closest.top, 0, height, 0, 1);
+      // bottom of closest pipe opening
+      inputs[2] = map(closest.bottom, 0, height, 0, 1);
+      // bird's y position
+      inputs[3] = map(this.y, 0, height, 0, 1);
+      // bird's y velocity
+      inputs[4] = map(this.velocity, -5, 5, 0, 1);
+
+      // Get the outputs from the network
+      let action = this.brain.predict(inputs);
+      // Decide to jump or not!
+      if (action[1] > action[0]) {
+        this.up();
+      }
+    }
+  }
+
+  // Jump up
+  up() {
+    this.velocity += this.lift;
+  }
+
+  bottomTop() {
+    // Bird dies when hits bottom?
+    return (this.y > height || this.y < 0);
+  }
+
+  // Update bird's position based on velocity, gravity, etc.
+  update() {
+    this.velocity += this.gravity;
+    // this.velocity *= 0.9;
+    this.y += this.velocity;
+
+    // Every frame it is alive increases the score
+    this.score++;
+  }
+}
